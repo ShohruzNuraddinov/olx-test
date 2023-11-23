ab836ed37bda55cb627c3c31a79cb253ac400803
Shohruz Nuraddinov <92042320+shohruzcoder@users.noreply.github.com>
2023-11-23 08:16:41 +0500

    initial commit

--- ads/admin.py
+++ ads/admin.py
@@ -24,14 +24,16 @@ class AdsAttributeValueInline(admin.TabularInline):
     inlines = [AdsAttrbuteOptionValueInline]
 
 
+@admin.register(Ads)
 class AdsAdmin(admin.ModelAdmin):
-    inlines = [AdsAttributeValueInline]
-    readonly_fields = ['created_at', 'updated_at', 'image_get',]
     list_per_page = 10
     date_hierarchy = 'created_at'
     change_list_template = "entities/ads_changelist.html"
+
     # image_display = AdminThumbnail(image_field='image')
     # image_display.short_description = 'Image'
+    readonly_fields = ['created_at', 'updated_at', 'image_get',]
+    inlines = [AdsAttributeValueInline]
     list_display = [
         'id',
         'title',
@@ -43,6 +45,7 @@ class AdsAdmin(admin.ModelAdmin):
         'created_at',
         'updated_at',
     ]
+
     list_filter = [
         'id',
         'title',
@@ -50,18 +53,16 @@ class AdsAdmin(admin.ModelAdmin):
         'created_at',
         'updated_at',
     ]
-
     search_fields = [
         'title',
         'sub_category__category__title',
         'content'
     ]
-
     actions = ['export_as_csv',
                'mark_is_vip', 'mark_is_top',
                'mark_not_is_top', 'mark_not_is_vip',
                ]
-    
+
     def get_urls(self):
         urls = super().get_urls()
         my_urls = [
@@ -86,11 +87,9 @@ class AdsAdmin(admin.ModelAdmin):
             url=obj.image.url,
             width=obj.image.width/10,
             height=obj.image.height/10,
-        )
-        )
+        ))
 
     def export_as_csv(self, request, queryset):
-
         meta = self.model._meta
         field_names = [field.name for field in meta.fields]
 
@@ -103,14 +102,12 @@ class AdsAdmin(admin.ModelAdmin):
         for obj in queryset:
             row = writer.writerow([getattr(obj, field)
                                   for field in field_names])
-
         return response
 
     def import_csv(self, request):
         if request.method == "POST":
             csv_file = request.FILES["csv_file"]
             reader = csv.reader(csv_file)
-            # Create Hero objects from passed in data
             # ...
             self.message_user(request, "Your csv file has been imported")
             return redirect("..")
@@ -121,6 +118,40 @@ class AdsAdmin(admin.ModelAdmin):
         )
 
 
-admin.site.register(Ads, AdsAdmin)
-admin.site.register(AdsAttributeValue)
-admin.site.register(AdsAttributeValueOption)
+@admin.register(AdsAttributeValue)
+class AdsAttributeValueAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'ads',
+        'value',
+    ]
+    list_filter = [
+        'id',
+        'ads',
+        'value',
+    ]
+    search_fields = [
+        'id',
+        'ads__title',
+        'value',
+    ]
+    inlines = [AdsAttrbuteOptionValueInline]
+
+
+@admin.register(AdsAttributeValueOption)
+class AdsAttributeValueOptionAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'ads_attribute_value',
+        'option',
+    ]
+    list_display = [
+        'id',
+        'ads_attribute_value',
+        'option',
+    ]
+    search_fields = [
+        'id',
+        'ads_attribute_value',
+        'option',
+    ]
